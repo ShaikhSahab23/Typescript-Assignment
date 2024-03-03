03523a2d97578bbc42b9b0a1bf3f7e67981db264
Muhammad Ahsan Sheikh <161168116+ShaikhSahab23@users.noreply.github.com>
2024-03-03 15:34:41 -0800

    Add files via upload

---
+++ 5 Famous_Quote_2.ts
@@ -0,0 +1,5 @@
+let famous_person : string = 'Muhammad Ali';
+
+let message : string = '“Service to others is the rent you pay for the room you have here on Earth.” ';
+
+console.log(`${famous_person} once said, ${message}`);
